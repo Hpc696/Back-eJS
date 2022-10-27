89a0f3100bf7d727c4668e07d700c78583f74c8b
Honrop696 <honropgiochi@gmail.com>
2022-10-27 15:36:30 +0200

    Login DAY1 ended

---
+++ Login.js
@@ -0,0 +1,4 @@
+function Hello(){
+    alert("Benvenuto " + document.getElementById("container").children[2].value + "  " + document.getElementById("container").children[6].value); 
+    alert( "Il tuo telefono è " + document.getElementById("container").children[10].value + " cojone" );
+}
